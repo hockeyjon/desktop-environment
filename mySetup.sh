eeea21dedae19c0764348f223b1309ac764f7d3d
Jon Kopp <jkopp@gogoair.com>
2016-07-26 11:50:42 -0600

    Update mySetup.sh

--- mySetup.sh
+++ mySetup.sh
@@ -6,5 +6,16 @@ mkdir git
 cd git
 git clone git@github.com:hockeyjon/ubuntu.git myUbuntu
 cp myUbuntu/bashrc $HOME/.bashrc
+
+
+#Install desktop image
 cp myUbuntu/BlueOrangePeel.jpg $HOME/Pictures/.
 gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/BlueOrangePeel.jpg
+
+#Install Dropbox
+rc = `uname -a | grep x86_64 | wc -l`
+if [ $rc -eq "1" ]; then 
+    cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    ~/.dropbox-dist/dropboxd
+fi
+
