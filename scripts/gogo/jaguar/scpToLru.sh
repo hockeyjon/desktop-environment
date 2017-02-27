5833055a8c3e01741b146afafb00d4158244dcb6
Jonathan Kopp <jkopp@gogoair.com>
2017-02-27 13:28:43 -0700

    Added echo's to scripts/gogo/jaguar/scpToLru.sh

--- scripts/gogo/jaguar/scpToLru.sh
+++ scripts/gogo/jaguar/scpToLru.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "cd ~/git/myWorkspace"
 cd ~/git/myWorkspace
+echo "git pull"
 git pull
+echo "tar cvzf scripts.tar.gz scripts"
 tar cvzf scripts.tar.gz scripts
+echo "docker cp scripts.tar.gz maint-cli:/tmp/."
 docker cp scripts.tar.gz maint-cli:/tmp/.
+echo "docker exec -it maint-cli scp /tmp/scripts.tar.gz root@172.20.1.1:."
 docker exec -it maint-cli scp /tmp/scripts.tar.gz root@172.20.1.1:.
