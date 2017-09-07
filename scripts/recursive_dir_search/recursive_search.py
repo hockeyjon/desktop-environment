5b16ff51de2cc7aa379780dd217090c0e4feb789
Jonathan Kopp <jkopp@gogoair.com>
2017-09-07 13:47:51 -0600

    recursive_search.py: changed file type to jpg.

--- scripts/recursive_dir_search/recursive_search.py
+++ scripts/recursive_dir_search/recursive_search.py
@@ -2,16 +2,18 @@ import os
 import shutil
 
 new_directory = "allFiles"
-file_type = "txt"
+file_type = "jpg"
 
 for root, directories, filenames in os.walk('.'):
     if filenames:
         if not os.path.exists(new_directory):
             os.makedirs(new_directory)
 
-    for filename in filenames:
-        if filename.endswith(file_type):
-            src = os.path.join(root, filename)
-            shutil.copy2(src, new_directory)
-        else:
-            print filename + " is not of type " + file_type
+        for filename in filenames:
+            if filename.endswith(file_type):
+                src = os.path.join(root, filename)
+                shutil.copy2(src, new_directory)
+            else:
+                print filename + " is not of type " + file_type
+    else:
+        print "Could not find any files."
