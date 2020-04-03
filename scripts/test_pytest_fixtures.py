c4493e51b81127a8276e6d3cf516121ff596346e
Jon Kopp <jkopp@freewave.com>
2020-04-03 09:13:48 -0600

    Added pytest fixtures example.

---
+++ scripts/test_pytest_fixtures.py
@@ -0,0 +1,38 @@
+import pytest
+
+def get_var():
+    return "got var"
+
+@pytest.fixture(scope="module")
+def set_up_first():
+    var1 = "set_up_first"
+    var3 = get_var()
+    fixture_dict = {"var1": var1,
+                    "var3": var3}
+
+    yield fixture_dict
+
+
+@pytest.fixture(scope="module")
+def set_up_second(set_up_first):
+    var2 = "set_up_second"
+
+    set_up_first.update({"var2": var2})
+
+    yield set_up_first
+
+
+def test_one(set_up_second):
+    assert set_up_second["var1"] == "set_up_first"
+
+def test_two(set_up_second):
+    assert set_up_second["var2"] == "set_up_second"
+
+def test_three(set_up_second):
+    assert set_up_second["var3"] == "got var"
+
+def test_four(set_up_second, set_up_first):
+    assert set_up_second["var2"] == "set_up_second"
+
+def test_five(set_up_second, set_up_first):
+        assert set_up_first["var1"] == "set_up_first"
