da6e61018e216e7f34ac8206073acafa0f9aa13a
Jon Kopp <jkopp@freewave.com>
2020-04-09 10:08:18 -0600

    test_pytest_fixtures.py: Added static helper class example

--- scripts/test_pytest_fixtures.py
+++ scripts/test_pytest_fixtures.py
@@ -1,5 +1,32 @@
 import pytest
 
+def static_init(cls):
+    """
+    This is implements the static init marker that returns a static class with
+    static class variables defined in the class' static_init() method.
+
+    :param cls: class that is calling it's static_init
+    :return: static instance of the class
+    """
+    if getattr(cls, "static_init", None):
+        cls.static_init()
+    return cls
+
+@static_init
+class PayloadBuilder():
+    """
+    Statuc class that Builds payloads to be used as the test body in simulated
+    HTTP requests from IOEX models
+
+    """
+
+    @classmethod
+    def static_init(cls):
+        payload = {}
+        setattr(cls, 'payload', payload)
+        payload.update(payload1 = "test_payload1")
+
+
 def get_var():
     return "got var"
 
@@ -27,21 +54,35 @@ def get_value_one():
 def get_value_two():
     return "two"
 
-def test_one(set_up_second):
-    assert set_up_second["var1"] == "set_up_first"
+class TestClass:
 
-def test_two(set_up_second):
-    assert set_up_second["var2"] == "set_up_second"
+    @classmethod
+    def setup_class(cls):
+        cls.second_val = "set_up_second"
 
-def test_three(set_up_second):
-    assert set_up_second["var3"] == "got var"
+    def test_one(self, set_up_second):
+        assert set_up_second["var1"] == "set_up_first"
 
-def test_four(set_up_second, set_up_first):
-    assert set_up_second["var2"] == "set_up_second"
+    def test_two(self, set_up_second):
+        assert set_up_second["var2"] == "set_up_second"
 
-def test_five(set_up_second, set_up_first):
-        assert set_up_first["var1"] == "set_up_first"
+    def test_three(self, set_up_second):
+        assert set_up_second["var3"] == "got var"
 
-@pytest.mark.parametrize("value", [get_value_one(), get_value_two(), "three"])
-def test_six(value):
-    assert "wrong" == value, "Failed on {}".format(value)
+    def test_four(self, set_up_second, set_up_first):
+        assert set_up_second["var2"] == "set_up_second"
+
+    def test_five(self, set_up_second, set_up_first):
+            assert set_up_first["var1"] == "set_up_first"
+
+    @pytest.mark.parametrize("value1,value2", [("one", get_value_one()),
+                                                       ("two", get_value_two()),
+                                                       ("three", "three")])
+    def test_six(self, value1, value2):
+        assert value1 == value2, "Failed on {}".format(value1)
+
+    parameter_values = [("test7", PayloadBuilder.payload["payload1"])]
+    @pytest.mark.parametrize("testid, payload", parameter_values)
+    def test_seven(self, testid, payload):
+        assert payload == "test_payload1", "{} failed".format(testid)
+
